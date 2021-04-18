c457a0c47bf271ea9b530d94bfb6c2604ad785c0
Nick Palmer <ylnickpalmer@gmail.com>
2021-04-18 02:07:45 -0700

    created basic layout for saved books page

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -1,7 +1,29 @@
+import { Container, Col, Row } from 'reactstrap'
+import './Saved.css'
+
 const Saved = () => {
   return (
     <>
-      <h1>This is the Home Page!</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Google Books Reading List</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Your Saved Books</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col id="started">
+            <h5>Started</h5>
+          </Col>
+          <Col id="unstarted">
+            <h5>Unstarted Books</h5>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }

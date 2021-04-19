41a5e774572302edebc92fed307dd9ded7f8a0be
Nick Palmer <ylnickpalmer@gmail.com>
2021-04-19 01:31:02 -0700

    created googleBooksAPI methods

--- client/src/utils/BookAPI/BookAPI.js
+++ client/src/utils/BookAPI/BookAPI.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const Book = {
-  getBook: search => axios.get(`/api/googleBooks/${search}`)
+  getGoogleBooks: search => axios.get(`/api/googleBooks/${search}`),
+  getBooks: () => axios.get('/api/books'),
+  addBook: book => axios.get('/api/books', book),
+  deleteBook: id => axios.get(`/api/books/${id}`)
 }
 
 export default Book
--- routes/googleBooksRoutes.js
+++ routes/googleBooksRoutes.js
@@ -4,10 +4,7 @@ const axios = require('axios')
 
 router.get('/googleBooks/:search', (req, res) => {
   axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.search}&maxResults=20&apikey=${process.env.googleBooksAPIKey}`)
-    .then(({ data: { items: books }}) => {
-      res.json(books)
-      res.sendStatus(200)
-    })
+    .then(({ data: { items: books }}) => res.json(books))
     .catch(err => console.log(err))
 })
 

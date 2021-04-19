146120e0741ad3344d249029755fedc335b04bab
Nick Palmer <ylnickpalmer@gmail.com>
2021-04-19 01:22:52 -0700

    created google books API routes

--- routes/googleBooksRoutes.js
+++ routes/googleBooksRoutes.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
 const { Book } = require('../models')
-import axios from 'axios'
+const axios = require('axios')
 
 router.get('/googleBooks/:search', (req, res) => {
-  
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.search}&maxResults=20&apikey=${process.env.googleBooksAPIKey}`)
+    .then(({ data: { items: books }}) => {
+      res.json(books)
+      res.sendStatus(200)
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
